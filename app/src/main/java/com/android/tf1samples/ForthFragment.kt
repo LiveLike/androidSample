ed8cbc225db29849ad51a98fbaf3fb388ba43382
Changdeo Jadhav <changdeo@livelike.com>
2024-05-29 12:47:18 +0530

    TNT Sample

---
+++ app/src/main/java/com/android/tf1samples/ForthFragment.kt
@@ -0,0 +1,161 @@
+package com.android.tf1samples
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.android.tf1samples.databinding.FragmentSecondBinding
+import com.livelike.engagementsdk.LiveLikeWidget
+import com.livelike.engagementsdk.fetchWidgetDetails
+import com.livelike.engagementsdk.publicapis.LiveLikeCallback
+import com.livelike.engagementsdk.widget.LiveLikeWidgetViewFactory
+import com.livelike.engagementsdk.widget.widgetModel.*
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_ThirdFragment)
+        }
+
+        binding.widgetView.showTimer = false
+//        binding.widgetView.enableDefaultWidgetTransition = false
+//        binding.widgetView.widgetViewFactory = object : LiveLikeWidgetViewFactory {
+//            override fun createAlertWidgetView(alertWidgetModel: AlertWidgetModel): View? {
+//                return null
+//            }
+//
+//            override fun createCheerMeterView(cheerMeterWidgetModel: CheerMeterWidgetmodel): View? {
+//                return null
+//            }
+//
+//            override fun createImageSliderWidgetView(imageSliderWidgetModel: ImageSliderWidgetModel): View? {
+//                return null
+//            }
+//
+//            override fun createNumberPredictionFollowupWidgetView(
+//                followUpWidgetViewModel: NumberPredictionFollowUpWidgetModel,
+//                isImage: Boolean
+//            ): View? {
+//                return null
+//            }
+//
+//            override fun createNumberPredictionWidgetView(
+//                numberPredictionWidgetModel: NumberPredictionWidgetModel,
+//                isImage: Boolean
+//            ): View? {
+//                return null
+//            }
+//
+//            override fun createPollWidgetView(
+//                pollWidgetModel: PollWidgetModel,
+//                isImage: Boolean
+//            ): View? {
+//                return activity?.let {
+//                    CustomPollWidget(it).apply {
+//                        this.pollWidgetModel = pollWidgetModel
+//                    }
+//                }
+//            }
+//
+//            override fun createPredictionFollowupWidgetView(
+//                followUpWidgetViewModel: FollowUpWidgetViewModel,
+//                isImage: Boolean
+//            ): View? {
+//                return null
+//            }
+//
+//            override fun createPredictionWidgetView(
+//                predictionViewModel: PredictionWidgetViewModel,
+//                isImage: Boolean
+//            ): View? {
+//                return null
+//            }
+//
+//            override fun createQuizWidgetView(
+//                quizWidgetModel: QuizWidgetModel,
+//                isImage: Boolean
+//            ): View? {
+//                return null
+//            }
+//
+//            override fun createSocialEmbedWidgetView(socialEmbedWidgetModel: SocialEmbedWidgetModel): View? {
+//                return null
+//            }
+//
+//            override fun createTextAskWidgetView(askWidgetViewModel: TextAskWidgetModel): View? {
+//                return activity?.let {
+//                    CustomTextAskWidget(it).apply {
+//                        this.askWidgetModel = askWidgetViewModel
+//                    }
+//                }
+//            }
+//
+//            override fun createVideoAlertWidgetView(videoAlertWidgetModel: VideoAlertWidgetModel): View? {
+//                return null
+//            }
+//
+//        }
+
+        loadTextPoll()
+//        loadTextAskWidget()
+    }
+
+    private fun loadTextPoll() {
+        (activity?.application as Application).sdk.fetchWidgetDetails("2d7f63cb-0ff0-4f0a-b3cf-81760d48be33",
+            "text-poll") { result, error ->
+            result?.let {
+                binding.widgetView.displayWidget(
+                    (activity?.application as Application).sdk,
+                    result, showWithInteractionData = true
+                )
+            }
+            error?.let {
+                Toast.makeText(activity?.applicationContext, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun loadTextAskWidget() {
+        (activity?.application as Application).sdk.fetchWidgetDetails("151359d2-de10-4e14-aae1-85edc32f50bc",
+            "text-ask"){result, error ->
+            result?.let {
+                binding.widgetView.displayWidget(
+                    (activity?.application as Application).sdk,
+                    result, showWithInteractionData = true
+                )
+            }
+            error?.let {
+                Toast.makeText(activity?.applicationContext, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
